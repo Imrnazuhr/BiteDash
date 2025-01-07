7d80894579cffe47c1163dc4d7a86fa6cc73bd0d
Imrnazuhr <imrnazuhr13@gmail.com>
2025-01-08 00:13:01 +0800

    add main page
    also minor adjust to css

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function navigateToPage(page) {
     window.location.href = page;
 }
+function registered() {
+    document.getElementById("registered-status").textContent = "Your account has been registered!";
+}
